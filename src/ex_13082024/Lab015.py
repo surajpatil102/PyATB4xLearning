c79a9c7bbd441cd1966a75c482efd02d6fbb346a
DELL <suraj.patil102@gmail.com>
2024-11-20 18:50:13 +0530

    Lab002 all the file added test

---
+++ src/ex_13082024/Lab015.py
@@ -0,0 +1,8 @@
+# to find the max number - functions
+
+result = max(3,4)
+print(result)
+
+# result1= max(34,44,'suraj') TypeError: '>' not supported between instances of 'str' and 'int'
+result1= min(123,223,100,12.2,1,-2)
+print(result1)
